cbbe0f28b68886239a8e073736db042dc0342fcf
oyeahmed <ahmed.shabbir1308@gmail.com>
2021-08-02 16:16:53 +0500

    rating

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
@@ -22,9 +21,7 @@ export default function ButtonAppBar() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
+
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
@@ -34,7 +31,7 @@ export default function ButtonAppBar() {
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
     marginRight: theme.spacing(2),
