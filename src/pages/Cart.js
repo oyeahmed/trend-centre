240ae9edae22823b5115021a73b6fd264184d378
oyeahmed <ahmed.shabbir1308@gmail.com>
2021-08-30 18:43:20 +0500

    cart final

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,21 @@
 import AppBar from "../components/AppBar";
 import UseFetch from "../hooks/UseFetch";
-import CartCard from "../components/CartCard";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import Paper from "@material-ui/core/Paper";
+import TableBody from "@material-ui/core/TableBody";
+import TableRow from "@material-ui/core/TableRow";
 
 export default function Cart() {
+  const classes = useStyles();
   const { loading, error, data } = UseFetch("http://localhost:1337/Carts");
+  let tp = 0;
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error...</p>;
-  console.log(data.length);
   return (
     <div>
       <AppBar />
@@ -22,18 +30,47 @@ export default function Cart() {
               marginTop: "10px",
             }}
           >
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="spanning table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <h2>Product Image</h2>
+                    </TableCell>
+                    <TableCell>
+                      <h2>Product Name</h2>
+                    </TableCell>
+                    <TableCell>
+                      <h2>Price</h2>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
             {data[0].products[0].add_to_cart ? (
               data.map((cartProduct) => (
-                <CartCard
-                  key={cartProduct.products[0].id}
-                  title={cartProduct.products[0].title}
-                  id={cartProduct.products[0].id}
-                  p1={cartProduct.products[0].original_price}
-                  S
-                  p2={cartProduct.products[0].discount_price}
-                  S
-                  image={cartProduct.products[0].image[0].name}
-                />
+                <TableContainer component={Paper}>
+                  <Table className={classes.table} aria-label="spanning table">
+                    <TableBody>
+                      <TableRow>
+                        <TableCell>
+                          <img
+                            style={{ height: "7em", width: "6em" }}
+                            src={cartProduct.products[0].image[0].name}
+                            alt="image"
+                          />
+                        </TableCell>
+                        <TableCell>{cartProduct.products[0].title}</TableCell>
+                        <TableCell>
+                          ${cartProduct.products[0].original_price}
+                        </TableCell>
+                      </TableRow>
+                      <p style={{ display: "none" }}>
+                        {(tp += cartProduct.products[0].original_price)}
+                      </p>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
               ))
             ) : (
               <h1>Cart is empty</h1>
@@ -46,7 +83,8 @@ export default function Cart() {
                 alignItems: "center",
                 justifyContent: "space-between",
                 marginLeft: "5em",
-                marginRight: "15em",
+                marginRight: "10em",
+                marginTop: "3em",
               }}
             >
               <Button
@@ -58,10 +96,10 @@ export default function Cart() {
                 Continue Shopping
               </Button>
               <h3>
-                Total Price: <span style={{ color: "green" }}>$184</span>{" "}
+                Total Price: <span style={{ color: "green" }}>${tp}</span>{" "}
               </h3>
             </div>
-            <div style={{ float: "right", marginRight: "15em" }}>
+            <div style={{ float: "right", marginRight: "10em" }}>
               <Button size="large" variant="outlined" color="primary">
                 Checkout
               </Button>
@@ -74,3 +112,8 @@ export default function Cart() {
     </div>
   );
 }
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
